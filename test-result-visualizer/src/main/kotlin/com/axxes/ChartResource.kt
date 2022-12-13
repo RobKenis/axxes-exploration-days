91f8eccf4795c87633a1d25ff6f7f9949a6cf145
Rob Kenis <rob.kenis@hotmail.com>
2022-12-13 15:09:24 +0100

    Write sick test for svg

--- test-result-visualizer/src/main/kotlin/com/axxes/ChartResource.kt
+++ test-result-visualizer/src/main/kotlin/com/axxes/ChartResource.kt
@@ -9,6 +9,7 @@ import java.awt.geom.Rectangle2D
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 
 
 @Path("/chart")
@@ -17,26 +18,11 @@ class ChartResource {
     @GET
     @Path("/200.svg")
     @Produces("image/svg+xml")
-    fun generate200m(): String {
+    fun generate200m(@QueryParam("first") first: Double, @QueryParam("second") second: Double, @QueryParam("testName") testName: String): String {
         val dataset = DefaultCategoryDataset()
-        dataset.addValue(106.0, "nov 22", "100m")
-        dataset.addValue(116.0, "nov 22", "200m")
-        dataset.addValue(106.0, "feb 22", "100m")
-        dataset.addValue(119.0, "feb 22", "200m")
-        val chart: JFreeChart = ChartFactory.createLineChart(
-                "200 Meters",
-                "Distance in meters",
-                "Time in seconds",
-                dataset,
-                PlotOrientation.VERTICAL,
-                true,
-                true,
-                true
-        )
-        val g2 = SVGGraphics2D(720.0, 480.0)
-        val area = Rectangle2D.Double(0.0, 0.0, 720.0, 480.0)
-        chart.draw(g2, area)
-        return g2.svgElement
+        dataset.addValue(first, testName, "100m")
+        dataset.addValue(second, testName, "200m")
+        return generateChart(dataset, "200 Meters")
     }
 
     @GET
@@ -52,8 +38,12 @@ class ChartResource {
         dataset.addValue(129.0, "feb 22", "200m")
         dataset.addValue(130.0, "feb 22", "300m")
         dataset.addValue(128.0, "feb 22", "400m")
+        return generateChart(dataset, "400 Meters")
+    }
+
+    private fun generateChart(dataset: DefaultCategoryDataset, title: String): String {
         val chart: JFreeChart = ChartFactory.createLineChart(
-                "400 Meters",
+                title,
                 "Distance in meters",
                 "Time in seconds",
                 dataset,
