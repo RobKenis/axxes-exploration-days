dd4157e9a103624295746996cd07a093a8741867
rrotthier <robbe.rotthier@axxes.com>
2022-12-13 15:45:37 +0100

    init

---
+++ spring-native/src/test/java/com/axxes/springnative/SpringNativeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.axxes.springnative;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
