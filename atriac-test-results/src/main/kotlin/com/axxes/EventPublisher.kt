35ecd80f8050598bd94c2cbc56e504a21494f30a
Cel Pynenborg <cel.pynenborg@telenetgroup.be>
2022-12-13 13:57:57 +0100

    Add logging when sending events and endpoint that fails

--- atriac-test-results/src/main/kotlin/com/axxes/EventPublisher.kt
+++ atriac-test-results/src/main/kotlin/com/axxes/EventPublisher.kt
@@ -1,11 +1,13 @@
 package com.axxes
 
+import io.quarkus.logging.Log
 import io.vertx.core.eventbus.EventBus
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class EventPublisher(val eventBus: EventBus) {
     fun <T> publish(event: Event<T>) {
+        Log.infov("Publishing event {0} to address [{1}]", event.payload(), event.address())
         eventBus.publish(event.address(), event.payload())
     }
 }
