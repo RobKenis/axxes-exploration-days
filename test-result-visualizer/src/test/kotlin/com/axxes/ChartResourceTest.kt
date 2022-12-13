5fbe886443faf47d16fc695bc815b96a70527fb3
Rob Kenis <rob.kenis@hotmail.com>
2022-12-13 15:19:48 +0100

    Fix the svg test on GitHub Actions

--- test-result-visualizer/src/test/kotlin/com/axxes/ChartResourceTest.kt
+++ test-result-visualizer/src/test/kotlin/com/axxes/ChartResourceTest.kt
@@ -3,6 +3,7 @@ package com.axxes
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.`when`
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
 import org.xmlunit.matchers.CompareMatcher.isIdenticalTo
 import org.xmlunit.placeholder.PlaceholderDifferenceEvaluator
 import java.nio.file.Files
@@ -10,6 +11,7 @@ import java.nio.file.Paths
 
 
 @QuarkusTest
+@DisabledIfEnvironmentVariable(named = "DISABLE_THE_SVG_TEST", matches = "true", disabledReason = "I don't even know why this doesn't work on GitHub")
 class ChartResourceTest {
 
     @Test
